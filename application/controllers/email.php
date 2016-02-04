e781effe4c3b6ba5d21b056490ee067e3c385596
TheLaughingGod1986 <benoats@gmail.com>
2016-02-04 15:46:55 +0000

    seting up email system

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -13,21 +13,36 @@ class Email extends CI_Controller
 {
     function __construct()
     {
-        parent::Controller();
+        parent::__construct();
     }
 
     function index()
     {
-        $config = Array(
+        $this->load->library('email');
+        $config = array(
             'protocol' => 'smtp',
             'smtp_host' => 'ssl://smtp.googlemail.com',
             'smtp_port' => 465,
             'smtp_user' => 'benoats@gmail.com',
             'smtp_pass' => 'Plymouth.09'
-
         );
-
         $this->load->library('email', $config);
-        
+
+        $this->email->set_newline("\r\n");
+
+        $this->email->from('benoats@gmail.com' , 'ben oats');
+        $this->email->to('benoats@gmail.com');
+        $this->email->subject('this is a test');
+        $this->email->message('it is working .... great!');
+
+        if($this->email->send())
+        {
+            echo 'your email was sent';
+        }
+
+        else
+        {
+            show_error($this->email->print_debugger());
+        }
     }
 }
