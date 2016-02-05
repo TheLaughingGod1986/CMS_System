e99d04781fe402e4fb5c2a7781b3bf3a58cdce62
TheLaughingGod1986 <benoats@gmail.com>
2016-02-05 05:06:21 +0000

    changed base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/advancedsystme/';
+$config['base_url'] = 'http://benoats.co/university/cms-system/';
 
 $config['server_root'] = $_SERVER['DOCUMENT_ROOT'];
 
